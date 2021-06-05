7e000e38c486b070935db74c589eb953d5e643a2
ishusaxena <85158528+ishusaxena@users.noreply.github.com>
2021-06-05 16:59:30 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js </h1>
+      <p>
+        this was a complete bootcamp of web development with Javascript and
+        React.js taken up by shaurya sinha .this bootacmp covered basics and
+        fundamentals right from scratch. After the completion of bootcamp ,a
+        project is also given.
+      </p>
+    </div>
+  );
+}
+export default Note;
